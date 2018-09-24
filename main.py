985dbcee506f5bfc3153511c06ca8d00ca83b1b3
Hi I'm Bugger <skordesign@outlook.com>
2018-09-24 18:06:59 +0700

    update

--- main.py
+++ main.py
@@ -4,17 +4,16 @@ from bs4 import BeautifulSoup
 import re
 import pandas as pd
 import os
+import time
 
 
 #launch url
-loginPath = "https://elements.envato.com/sign-in"
 url = "https://elements.envato.com/stock-video"
 
 # create a new Firefox session
 driver = webdriver.Chrome()
 driver.implicitly_wait(30);
-driver.get(loginPath)
-
-
-driver.implicitly_wait(30);
 driver.get(url)
+
+
+
